fea099fb0b960302aebcb2cebbab3bb09e4616f0
Ben Pinali <ben@benpinali.com>
2016-08-25 18:58:05 -0500

    Added scales.

--- scripts/main.js
+++ scripts/main.js
@@ -48,6 +48,10 @@ scales['mixolydian'] = Array(0, 2, 4, 5, 7, 9, 10);
 scales['aeolian'] = Array(0, 2, 3, 5, 7, 8, 10);
 scales['locrian'] = Array(0, 1, 3, 5, 6, 8, 10);
 scales['whole-tone'] = Array(0, 2, 4, 6, 8, 10);
+scales['whole-half-diminished'] = Array(0, 2, 3, 5, 6, 8, 9, 11);
+scales['half-whole-diminished'] = Array(0, 1, 3, 4, 6, 7, 9, 10);
+scales['chromatic'] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+
 
 stringTuning = Array();
 stringTuning['standard'] = Array('E', 'B', 'G', 'D', 'A', 'E');
